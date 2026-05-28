957b58db1e47dab0997065daa33d0ddbc1eb745d
kurgeri <b3kurger@vasvari.org>
2026-05-28 11:54:42 +0200

    Stopper

--- teszt.js
+++ teszt.js
@@ -3,11 +3,21 @@ let teszt = document.getElementById("test")
 teszt.style.display = "none"
 
 let teszt_start = document.getElementById("test_start_button");
-teszt_start.addEventListener("click", () => {
+// let idozito = document.getElementById(idozito);
+
+// function startStopper()
+// {
+    
+// }
+
+
+teszt_start.addEventListener("click", () =>
+{
 teszt.style.display = "block"
 document.getElementById("szemelyes_adatok").style.display = "none"
-teszt_start.display = "none";
+
 });
+
 reset_gomb.addEventListener("click", () => {
 location.replace(location.href)
 })
